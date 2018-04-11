d208d4dbd636461e15ec0fd29ddb94045a02a101
“lahirusamishka” <lahirusamishka@gmail.com>
2018-04-11 16:20:24 +0530

    seller-sign-up

--- FurniRent/Front-End/src/app/app.module.ts
+++ FurniRent/Front-End/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SellerComponent } from './seller_signup/seller.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SellerComponent
   ],
   imports: [
     BrowserModule
