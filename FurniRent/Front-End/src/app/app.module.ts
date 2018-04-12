12923430fb86e016d4d34929494aacce56f1afda
TharakaCz <tharakacz823@gmail.com>
2018-04-12 22:24:43 +0530

    nav

--- FurniRent/Front-End/src/app/app.module.ts
+++ FurniRent/Front-End/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ViewComponent } from './view/view.component';
+import { NavBarComponent } from './view/nav-bar/nav-bar.component';
+import { SliderComponent } from './view/slider/slider.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponent,
+    NavBarComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule
